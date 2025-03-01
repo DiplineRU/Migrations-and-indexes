3194ce822908d3ca540e2f22b29a51e381e1d950
DiplineRU <artgul52@gmail.com>
2025-03-01 15:14:02 +0300

    add test/pom/application

---
+++ src/test/java/com/example/Migrations_and_indexes/MigrationsAndIndexesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Migrations_and_indexes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationsAndIndexesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
